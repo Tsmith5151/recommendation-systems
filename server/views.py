78fce362eddfd7921a7487078b52cf7d679dd43d
Trace Smith <tsmith5151@gmail.com>
2021-07-06 10:51:49 -0500

    pep8 formatting

--- server/views.py
+++ server/views.py
@@ -8,25 +8,26 @@ from recommender.database import utils as db_main
 DATABASE_ENV = os.environ["DATABASE_ENV"]
 TABLE = os.environ["RESULTS_TABLE"]
 
+
 class SimilarUsers:
-    
-    def __init__(self, user:str):
+    def __init__(self, user: str):
         self.user = user
-        
+
     def fetch_user_from_db(self):
-        """ Fetch User Record from SQLite Database"""
+        """Fetch User Record from SQLite Database"""
         query = f"select * from {TABLE} where user_handle = {self.user}"
-        print('Table',TABLE)
+        print("Table", TABLE)
         return db_main.read_table(DATABASE_ENV, query)
 
     def get_payload(self):
-        """ Return JSON Payload containing Input User and Top 
+        """Return JSON Payload containing Input User and Top
         Similar Users with associated similarity scores"""
         data = self.fetch_user_from_db()
         if data.shape[0] == 0:
-            return {self.user_id: 'No records found!'}
+            return {self.user_id: "No records found!"}
         else:
-            return ({str(self.user): list(data.loc[0].values.flatten()[:-1])})
+            return {str(self.user): list(data.loc[0].values.flatten()[:-1])}
+
 
 @app.route("/api/similarity/", methods=["POST", "GET"])
 def get_user_similarity():
