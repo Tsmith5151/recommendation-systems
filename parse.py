4392711966487d5518dd54160586aabdae8936dc
Trace Smith <tsmith5151@gmail.com>
2021-07-05 23:04:03 -0500

    update input arg parsers

--- parse.py
+++ parse.py
@@ -20,10 +20,29 @@ def get_parser() -> argparse:
         help="Environment for SQLite Database: dev/prod",
     )
     parser.add_argument(
+        "--similarity_metric",
+        default='cosine',
+        type=str,
+        help="Similarity metric for pairwise distance measurement",
+    )
+    parser.add_argument(
+        "--weights",
+        default=['0.50','0.30','0.20'],
+        action='store',
+        nargs="+",
+        help="Weights for similarity matrix: interest,assesssment,tags",
+    )
+    parser.add_argument(
         "--results_table",
         default=None,
         type=str,
         help="SQLite3 table containing user similarity metrics",
+    )
+    parser.add_argument(
+        "--user_id",
+        default=None,
+        type=str,
+        help="unique user id for scoring similarities",
     )
     parser.add_argument(
         "--hostname",
