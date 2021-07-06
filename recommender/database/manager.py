55c024f28a46a82d64ce25ebea006cb67b7b94f8
Trace Smith <tsmith5151@gmail.com>
2021-07-05 19:25:18 -0500

    add database manager class for handling connections to sqlite database; add helper functions for creating and read/write from tables

---
+++ recommender/database/manager.py
@@ -0,0 +1,29 @@
+import os
+import yaml
+import sqlite3
+import logging
+
+
+class DatabaseManager:
+    """Context manager for handling connections to SQLite Database"""
+
+    def __init__(self, env: str):
+        self.env = env
+        self.database_path = os.path.join("data", "sqlite3")
+
+    def __enter__(self):
+        try:
+            with open("configs.yml") as f:
+                params = yaml.safe_load(f)[self.env]
+            self.conn = sqlite3.connect(
+                os.path.join(self.database_path, f"{params['database']}.db")
+            )
+            return self.conn
+        except Exception as e:
+            logging.critical(f"Error connecting to database: {str(e)}")
+
+    def __exit__(self, e_type, e_val, _):
+        try:
+            self.conn.close()
+        except Exception as e:
+            logging.critical(f"Error closing database: {str(e)}")
