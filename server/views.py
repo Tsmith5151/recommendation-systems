ed0cce1e0439fb17275be94580934fc05e16254a
Trace Smith <tsmith5151@gmail.com>
2021-07-05 23:02:18 -0500

    setup flask-restful application; make a request to the server with provider user id and return json object with the top 5 most similar users along with the corresponding similarity score

---
+++ server/views.py
@@ -0,0 +1,40 @@
+import os
+import json
+import pandas as pd
+from server import app
+from flask import request
+from recommender.database import utils as db_main
+
+DATABASE_ENV = os.environ["DATABASE_ENV"]
+TABLE = os.environ["RESULTS_TABLE"]
+
+class SimilarUsers:
+    
+    def __init__(self, user:str):
+        self.user = user
+        
+    def fetch_user_from_db(self):
+        """ Fetch User Record from SQLite Database"""
+        query = f"select * from {TABLE} where user_handle = {self.user}"
+        return db_main.read_table(DATABASE_ENV, query)
+
+    def get_payload(self):
+        """ Return JSON Payload containing Input User and Top 
+        Similar Users with associated similarity scores"""
+        data = self.fetch_user_from_db()
+        if data.shape[0] == 0:
+            return {self.user_id: 'No records found!'}
+        else:
+            return ({str(self.user): list(data.loc[0].values.flatten()[:-1])})
+
+@app.route("/api/similarity/", methods=["POST", "GET"])
+def get_user_similarity():
+    user = json.loads(request.get_data())["user_handle"]
+    SU = SimilarUsers(user)
+    results = SU.get_payload()
+    return results
+
+
+# @app.errorhandler(500)
+# def index():
+#     return "Error with Flask Server", 500
