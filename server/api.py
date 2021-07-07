4d5a1392c7e4fc0b9f8e2512b7850f12bad8cedd
Trace Smith <tsmith5151@gmail.com>
2021-07-07 11:56:07 -0500

    return payload as json object in api call

--- server/api.py
+++ server/api.py
@@ -1,9 +1,10 @@
 import json
 import requests
+import flask
 from parse import get_parser
 
 
-def similarity(user_id: str, host: str = "0.0.0.0", port: int = 5000) -> json:
+def similarity(user_id: str, host: str = "0.0.0.0", port: int = 5000):
 
     """API call to flask app running on localhost
     and fetch top similar customers to the input customer(s)
@@ -12,8 +13,7 @@ def similarity(user_id: str, host: str = "0.0.0.0", port: int = 5000) -> json:
     to_json = json.dumps({"user_handle": user_id})
     headers = {"content-type": "application/json", "Accept-Charset": "UTF-8"}
     response = requests.post(url, data=to_json, headers=headers)
-    print(response.text)
-
+    return response.text
 
 if __name__ == "__main__":
     args = get_parser().parse_args()
