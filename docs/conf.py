de3deb1d4c48833e5c9eb68adacdfd8b79455cf2
Trace Smith <tsmith5151@gmail.com>
2021-07-11 15:56:13 -0500

    pep8 format

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,15 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+
+sys.path.insert(0, os.path.abspath("."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'user-recommender-system'
-copyright = '2021, Trace Smith'
-author = 'Trace Smith'
+project = "user-recommender-system"
+copyright = "2021, Trace Smith"
+author = "Trace Smith"
 
 
 # -- General configuration ---------------------------------------------------
@@ -33,7 +34,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
     "sphinx.ext.napoleon",
-	"m2r2"
+    "m2r2",
 ]
 #
 # source_suffix = ['.rst', '.md']
@@ -43,12 +44,12 @@ extensions = [
 master_doc = "index"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -56,12 +57,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-import os 
-env_device_name = os.getenv("DATABASE_DEV")
+html_static_path = ["_static"]
